74fc9ca44f9d7539a6dce2b6d53d570db8471fe4
Veritas <paralyzedsjy7@gmail.com>
2024-03-07 22:15:23 +0800

    version2

--- src/Berth.cpp
+++ src/Berth.cpp
@@ -8,34 +8,6 @@ int Berth::getId() const {
     return id_;
 }
 
-int Ber
-
-
-
-
-
-
-
-
-#include "robot.h"
-
-Robot::Robot(int id, const Position& pos) : id_(id), pos_(pos) ,carry_(false) {}
-
-Robot::Robot() : id_(0), pos_() ,carry_(false) {}
-
-int Robot::getId() const {
-	return id_;
-}
-
-Position Robot::getPosition() const {
-	return pos_;
-}
-
-bool Robot::hasGoods() const {
-	return carry_;
-}
-
-void Robot::carryGoods(Goods &goods) {
-	goods_ = goods;
-	carry_ = true;
+int Berth::getLoading_speed() const{
+    
 }
