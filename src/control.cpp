f5d72650680c6e487ef8741d633d9989c203da6b
Veritas <paralyzedsjy7@gmail.com>
2024-03-08 14:15:18 +0800

    version8(moveRobot函数)

--- src/control.cpp
+++ src/control.cpp
@@ -1,2 +1,13 @@
 #include "model.h"
 #include "util.h"
+
+bool moveRobort(Robot robot,int i){
+	int new_x = robot.getPosition().x + dx[i];
+    int new_y = robot.getPosition().y + dy[i];
+    if (!Robort_isCollision(Position(new_x, new_y))){
+		map[robot.getPosition().x][robot.getPosition().y] = '.';
+		map[new_x][new_y] = 'A';
+        return true;
+	}
+    else return false;
+}
